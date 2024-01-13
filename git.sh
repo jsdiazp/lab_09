c3205536690a285fbbd850c45d0c8851c99f31b5
Joan Sebastian Diaz del Castillo Posso <joan.diazdelcastillo@omnitrek.co>
2024-01-13 11:04:09 -0500

    Inicio de proyecto

---
+++ git.sh
@@ -0,0 +1,25 @@
+# git
+# Herramienta para controlar versionamientos
+#
+# Beneficios
+# - Establecer versiones de mi código para comprarlo en el tiempo
+# -- Conocer que ha cambiado en mi código, lo cual puede impactar en un error que
+#    se este presentado facilitando la depuración del código
+# -- Registrar las actividades que estoy ejecutando en el código
+# - Centralizar y respaldar mi código
+
+# init
+# Permite activar la herramienta git en una proyecto (carpeta)
+git init
+
+# status
+# Conocer el estado de mi repositorio git
+git status
+
+# add
+# Empezar a seguir los cambios de un archivo(s)
+git add git.sh # Voy a seguir los cambios de git.sh
+
+# commit
+# Registrar los cambios de un archivo(s)
+git commit -m"Inicio de proyecto" # Registro un cambio (versión) con un mensaje
